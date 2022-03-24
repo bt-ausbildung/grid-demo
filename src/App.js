3252bfc450abc3115dde165ef6b8f1c8f3504acd
Nikolaos Geladaris <nikolaos.geladaris@biteno.com>
2022-03-24 11:13:22 +0100

    <deploy initial commit>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="parent">
+<div className="header">header </div>
+<div className="content">content </div>
+<div className="footer">footer </div>
+</div>
+  );
+}
+
+export default App;
