69f97d5f83139d31876465351d023ae08a573460
Nikolaos Geladaris <nikolaos.geladaris@biteno.com>
2022-03-25 09:47:20 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return <div className="parent">
+  <div className="header">Header </div>
+  <div className="content inhalt">
+      <div class="ordner">Ordner</div>
+      <div class="dokumente">Dokumente</div>
+  </div>
+  <div className="footer">Footer </div>
+</div>
+}
+
+export default App;
